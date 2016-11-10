cda7c6091dd4b8cb1eae0b1823edb2fb5fd7e0f3
Dan Purdy <danjpurdy@gmail.com>
2016-11-10 14:35:58 +0000

    Add application structure

---
+++ app/bootstrap.js
@@ -0,0 +1,31 @@
+// ==============================================================================
+//
+//  bootstrap.js
+//
+// ==============================================================================
+
+// Import polyfills for es6 to es5
+import 'babel-polyfill';
+
+// Import angular
+import 'angular';
+
+// Import our app module
+import apModule from './app';
+
+// Core - Models
+import './core/model/model.config';
+
+// Core - Services
+import './core/services/services.config';
+
+// app config and run settings
+import './app.config';
+import './app.run';
+
+// bootstrap angular
+angular.element(document).ready(() => {
+  angular.bootstrap(document, [apModule.name], {
+    strictDi: true,
+  });
+});
--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -2,7 +2,7 @@ const webpack = require('webpack');
 const devConfig = require('./webpack.config.js');
 
 const define = new webpack.DefinePlugin({
-  __API_URL__: '\'http://localhost:8080/api\'',
+  __API_URL__: '\'https://localhost:8080/api\'',
   __APP_URL__: '\'https://localhost:8000\'',
 });
 devConfig.cache = true;
