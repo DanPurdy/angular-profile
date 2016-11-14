1c921464a055519a09146c7943bc50ad92c77bae
Dan Purdy <danjpurdy@gmail.com>
2016-11-14 16:45:28 +0000

    :white_check_mark: fix email valid tests

--- app/components/ap-email-valid/ap-email-valid.spec.js
+++ app/components/ap-email-valid/ap-email-valid.spec.js
@@ -23,7 +23,7 @@ describe('apEmailValid', () => {
 
     const element = angular.element(
       `<form name="testForm">
-          <input ng-model="model.testVal" id="testID" name="inputTest" ap-pass-valid>
+          <input ng-model="model.testVal" id="testID" name="inputTest" ap-email-valid>
         </form>`);
     $compile(element)($scope);
     $rootScope.$digest();
@@ -35,78 +35,35 @@ describe('apEmailValid', () => {
       expect(form.inputTest.$validators).to.not.be.empty;
     });
 
-    it('should have custom passLength validator assigned', () => {
-      expect(form.inputTest.$validators.passLength).to.not.be.undefined;
+    it('should have custom emailValid validator assigned', () => {
+      expect(form.inputTest.$validators.emailValid).to.not.be.undefined;
     });
 
     it('should have custom passFormat validator assigned', () => {
-      expect(form.inputTest.$validators.passFormat).to.not.be.undefined;
+      expect(form.inputTest.$validators.emailValid).to.not.be.undefined;
     });
   });
 
-  describe('$validator passLength', () => {
+  describe('$validator emailValid', () => {
     it('should not be valid when a value of 0 characters is supplied', () => {
       form.inputTest.$setViewValue('');
-      expect(form.inputTest.$error.passLength).to.be.true;
+      expect(form.inputTest.$error.emailValid).to.be.true;
     });
 
     it('should not be valid when a value less than 4 characters is supplied', () => {
       form.inputTest.$setViewValue('tes');
-      expect(form.inputTest.$error.passLength).to.be.true;
+      expect(form.inputTest.$error.emailValid).to.be.true;
     });
 
-    it('should be valid when a value 4 characters long is supplied', () => {
-      form.inputTest.$setViewValue('test');
-      expect(form.inputTest.$error.passLength).to.be.undefined;
+    it('should be valid when a valid email is supplied', () => {
+      form.inputTest.$setViewValue('test@test.dev');
+      expect(form.inputTest.$error.emailValid).to.be.undefined;
     });
 
-    it('should be valid when a value more than 8 characters long is supplied', () => {
-      form.inputTest.$setViewValue('testtesttest');
-      expect(form.inputTest.$error.passLength).to.be.undefined;
+    it('should not be valid when an invalid email is passed', () => {
+      form.inputTest.$setViewValue('testtes@.');
+      expect(form.inputTest.$error.emailValid).to.be.true;
     });
-  });
-
-  describe('$validator passFormat', () => {
-    it('should not be valid when no value supplied', () => {
-      form.inputTest.$setViewValue('');
-      expect(form.inputTest.$error.passFormat).to.be.true;
-    });
-
-    it('should not be valid when only lower case letters are supplied', () => {
-      form.inputTest.$setViewValue('test');
-      expect(form.inputTest.$error.passFormat).to.be.true;
-    });
-
-    it('should not be valid when only upper case letters are supplied', () => {
-      form.inputTest.$setViewValue('TEST');
-      expect(form.inputTest.$error.passFormat).to.be.true;
-    });
-
-    it('should not be valid when only numbers are supplied', () => {
-      form.inputTest.$setViewValue('1234');
-      expect(form.inputTest.$error.passFormat).to.be.true;
-    });
-
-    it('should not be valid when only lower case and upper case letters are supplied', () => {
-      form.inputTest.$setViewValue('tT');
-      expect(form.inputTest.$error.passFormat).to.be.true;
-    });
-
-    it('should not be valid when only lower case letters and numbers are supplied', () => {
-      form.inputTest.$setViewValue('12');
-      expect(form.inputTest.$error.passFormat).to.be.true;
-    });
-
-    it('should not be valid when only upper case letters and numbers are supplied', () => {
-      form.inputTest.$setViewValue('T1');
-      expect(form.inputTest.$error.passFormat).to.be.true;
-    });
-
-    it('should be valid when one lower case letter, upper case letter and number are supplied',
-      () => {
-        form.inputTest.$setViewValue('tT1');
-        expect(form.inputTest.$error.passFormat).to.be.undefined;
-      });
   });
 });
 
